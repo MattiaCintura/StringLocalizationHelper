70c3198ea95a9c72e37c6dd4f4e6c35e4382a783
Mattia Cintura <mattiacintura@icloud.com>
2022-11-15 20:33:56 +0100

    ADD: duplicate file class

---
+++ src/duplicate_file.py
@@ -0,0 +1,13 @@
+import json
+
+class DuplicateFile:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def open_file(self) -> dict:
+        with open(self.file_path, "r") as f:
+            return json.load(f)
+        
+    def save_file(self, data: dict, filename: str = 'new_file.json'):
+        with open(filename, 'w') as nf:
+            json.dump(data, nf, indent=2)
